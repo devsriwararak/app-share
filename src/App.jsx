ce0f03375a5d3abbb4ded26e4f0adda37a7bd619
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-10-25 16:55:16 +0700

    "Revert "test-01""
    This reverts commit 412e191cec13b451447d342dcd91a5b8a9cd2de8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { lazy, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { themeChange } from "theme-change";
+// import checkAuth from './app/auth';
+// import initializeApp from './app/init';
 
-const App = () => {
+// Importing pages
+const Layout = lazy(() => import("./containers/Layout"));
+const Login = lazy(() => import("./pages/Login"));
+// const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+// const Register = lazy(() => import('./pages/Register'))
+// const Documentation = lazy(() => import('./pages/Documentation'))
+
+// Initializing different libraries
+// initializeApp()
+
+// Check for login and initialize axios
+// const token = checkAuth()
+
+function App() {
+  useEffect(() => {
+    // 👆 daisy UI themes initialization
+    themeChange(false);
+  }, []);
+
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline md:no-underline">
-      Hello world!
-    </h1>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/documentation" element={<Documentation />} /> */}
+
+        {/* Place new routes over this */}
+        <Route path="/app/*" element={<Layout />} />
+
+        {/* <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/> */}
+          <Route path="*" element={<Navigate to='/app/welcome' replace />}/>
+
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
