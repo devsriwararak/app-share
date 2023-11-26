784f0a6340f5f9804525a545f7a0988398952f64
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-11-26 12:53:01 +0700

    26-11-2566-v-2

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const User = () => {
   return (
     <div>
-      <h1 className='text-3xl'>USER PAGE </h1>
+      <h1 className='text-3xl'>USER PAGE ! </h1>
       <p className='text-lg'>ถาพรวม จะดำเนินการในงวดที่ 3/3 ครับ</p>
     </div>
   )
