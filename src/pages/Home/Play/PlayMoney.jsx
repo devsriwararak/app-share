7bbc22624051e2988efc97061c0e456c214f3bac
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-11-28 00:42:18 +0700

    28-11-2566-v-1

---
+++ src/pages/Home/Play/PlayMoney.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HiOutlineCash } from "react-icons/hi";
+
+const PlayMoney = () => {
+  return (
+    <div>
+      <h2 className="text-lg text-black font-bold flex gap-2 items-center">
+        <HiOutlineCash size={22} /> รายละเอียดวงแชร์
+      </h2>
+    </div>
+  );
+};
+
+export default PlayMoney;
---
+++ src/pages/Home/Play/PlaySetting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HiOutlinePlay } from 'react-icons/hi'
+
+const PlaySetting = () => {
+  return (
+    <div>
+    <h2 className='text-lg text-black font-bold flex gap-2 items-center'><HiOutlinePlay size={22}/> รายละเอียดวงแชร์</h2>
+</div>
+  )
+}
+
+export default PlaySetting
