c51b8064596698c7cff8798c2a0fdc9b172903c1
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-12-18 14:24:59 +0700

    18-12-2566-v-3

--- src/pages/AdminMain/User/UserData.jsx
+++ src/pages/AdminMain/User/UserData.jsx
@@ -1,12 +1,19 @@
 import { Card, CardBody, CardHeader } from "@material-tailwind/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { HiOutlinePhotograph } from "react-icons/hi";
+import ModalDetaUser from "./ModalDetaUser";
 
 const UserData = ({ dataToModal }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   useEffect(() => {
     console.log(dataToModal);
   }, [dataToModal]);
   return (
     <>
+    <ModalDetaUser open={open} handleOpen={handleOpen} dataToModal={dataToModal} />
       <Card className="w-full mt-8 md:mt-0">
         <CardHeader className="flex justify-center py-2.5 bg-purple-400 text-white font-semibold">
           ข้อมูลลูกแชร์
@@ -39,27 +46,52 @@ const UserData = ({ dataToModal }) => {
 
           {dataToModal?.id}
 
-          {dataToModal?.id === 9 && (
+          {dataToModal?.id === 9 && dataToModal?.id !== undefined  && (
             <div className="bg-green-500 p-2 mt-4 rounded-md px-4">
-              <h1 className="text-2xl text-white">+ 1,000</h1>
+              <div className="flex justify-between items-center">
+                <h1 className="text-2xl text-white">+ 1,000</h1>
+                <HiOutlinePhotograph
+                  size={25}
+                  color="white"
+                  className=" cursor-pointer"
+                  onClick={handleOpen}
+                />
+              </div>
               <hr className="border-1 border-gray-400 mt-2" />
               <h4 className="mt-3 text-white text-lg">มือเป็น : 1,200 </h4>
               <h4 className="mt-3 text-white text-lg">มือตาย : 200 </h4>
             </div>
           )}
 
-          {dataToModal?.id === 8 && (
+          {dataToModal?.id === 8 && dataToModal?.id !== undefined && (
             <div className="bg-red-500 p-2 mt-4 rounded-md px-4">
-              <h1 className="text-2xl text-white">- 400</h1>
+              <div className="flex justify-between items-center">
+                <h1 className="text-2xl text-white">- 400</h1>
+                <HiOutlinePhotograph
+                  size={25}
+                  color="white"
+                  className=" cursor-pointer"
+                  onClick={handleOpen}
+                />
+              </div>
               <hr className="border-1 border-gray-400 mt-2" />
               <h4 className="mt-3 text-white text-lg">มือเป็น : 100 </h4>
               <h4 className="mt-3 text-white text-lg">มือตาย : 500 </h4>
             </div>
           )}
 
-{dataToModal?.id != 9 && dataToModal?.id != 8 && (
+          {dataToModal?.id != 9 && dataToModal?.id != 8 && dataToModal?.id !== undefined && (
             <div className="bg-orange-600 p-2 mt-4 rounded-md px-4">
-              <h1 className="text-2xl text-white">(+/-) 0</h1>
+              <div className="flex justify-between items-center">
+                <h1 className="text-2xl text-white">(+/-) 0</h1>
+                <HiOutlinePhotograph
+                  size={25}
+                  color="white"
+                  className=" cursor-pointer"
+                  onClick={handleOpen}
+                />
+              </div>
+
               <hr className="border-1 border-gray-400 mt-2" />
               <h4 className="mt-3 text-white text-lg">มือเป็น : 0 </h4>
               <h4 className="mt-3 text-white text-lg">มือตาย : 0 </h4>
@@ -73,7 +105,22 @@ const UserData = ({ dataToModal }) => {
           วันที่กิจกรรม
         </CardHeader>
 
-        <CardBody>1111</CardBody>
+        <CardBody>
+            <p className="text-black text-lg">18 ธ.ค. 2566</p>
+            <ul>
+                <li>- 217(วง สีม่วง บิทลดต้น 104,720) </li>
+                <li>- (ง.19/22) [104,720] </li>
+            </ul>
+            <hr className="mt-2 border-gray-300" />
+
+            <p className="text-black text-lg mt-2">18 ธ.ค. 2566</p>
+            <ul>
+                <li>- 217(วง สีม่วง บิทลดต้น 104,720) </li>
+                <li>- (ง.19/22) [104,720] </li>
+            </ul>
+            <hr className="mt-2 border-gray-300" />
+
+        </CardBody>
       </Card>
 
       <Card className="w-full mt-8 md:mt-0">
@@ -81,7 +128,21 @@ const UserData = ({ dataToModal }) => {
           วันที่จบวงแชร์
         </CardHeader>
 
-        <CardBody>1111</CardBody>
+        <CardBody>
+        <p className="text-black text-lg">18 ธ.ค. 2566</p>
+            <ul>
+                <li>- 217(วง สีม่วง บิทลดต้น 104,720) </li>
+                <li>- (ง.19/22) [104,720] </li>
+            </ul>
+            <hr className="mt-2 border-gray-300" />
+
+            <p className="text-black text-lg mt-2">18 ธ.ค. 2566</p>
+            <ul>
+                <li>- 217(วง สีม่วง บิทลดต้น 104,720) </li>
+                <li>- (ง.19/22) [104,720] </li>
+            </ul>
+            <hr className="mt-2 border-gray-300" />
+        </CardBody>
       </Card>
     </>
   );
